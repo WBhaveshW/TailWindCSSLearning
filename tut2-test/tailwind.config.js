4a1d500e3329e5a75f69b5d07c209605dfa290fc
Bhavesh <bhavesh.warekar@criticalog.com>
2024-10-15 23:12:59 +0530

    First Tailwindcss learning

---
+++ tut2-test/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// npx tailwindcss -i tut2-test.css -o output.css --watch
+
